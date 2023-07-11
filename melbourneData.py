35900d067629726bd551f3d81f3da4f1ae2b58a2
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-07-11 20:37:10 +0700

    Added model validation.
    Modifications qui seront validées :
    	modifié :         melbourneData.py

--- melbourneData.py
+++ melbourneData.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 melbourneData = pd.read_csv('./melb_data.csv').dropna()
 print(melbourneData.columns)
@@ -8,8 +10,8 @@ y = melbourneData.Price
 feats = ['Rooms', 'Bathroom', 'Bedroom2', 'Landsize', 'BuildingArea', 'YearBuilt']
 X = melbourneData[feats]
 
+train_X, test_X, train_y, test_y = train_test_split(X, y ,random_state=1)
 melbourneModel = DecisionTreeRegressor(random_state=1)
-melbourneModel.fit(X, y)
-print(y.head())
-print('-----------')
-print(melbourneModel.predict(X.head()))
+melbourneModel.fit(train_X, train_y)
+predictedPrice = melbourneModel.predict(test_X)
+print(mean_absolute_error(predictedPrice, test_y))
