07981bbbf1db5fb3f85d39472888f78504e9e8bb
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-07-17 21:55:25 +0700

    Added function to calculate abbreviated numeric values.
    Modifications qui seront validées :
    	modifié :         gamedata.py

--- gamedata.py
+++ gamedata.py
@@ -1,8 +1,26 @@
 import pandas as pd
+from pandas.core.series import Series
+from sklearn.model_selection import train_test_split
+
+
+def calculateKilo(col: Series, naReplacemane: int = 0) -> Series:
+    newcol = (col.replace(r'[KM]+$', '', regex=True).astype(float) *
+              col.str.extract(r'[\d\.]+([KM]+)', expand=False)
+              .fillna(naReplacemane)
+              .replace(['K', 'M'], [10 ** 3, 10 ** 6]).astype(int)
+              )
+
+    return newcol
+
 
 gameData = pd.read_csv('./backloggd_games.csv')
-gameData.Plays = (gameData.Plays.replace(r'[KM]+$', '', regex=True).astype(float) *
-                  gameData.Plays.str.extract(r'[\d\.]+([KM]+)', expand=False)
-                        .fillna(1)
-                        .replace(['K','M'], [10**3, 10**6]).astype(int)
-                  )
+
+# preprocess abbreviated numeric values
+gameData.Plays = calculateKilo(gameData.Plays, 0)
+gameData.Playing = calculateKilo(gameData.Playing, 0)
+gameData.Backlogs = calculateKilo(gameData.Backlogs, 0)
+gameData.Wishlist = calculateKilo(gameData.Wishlist, 0)
+gameData.Lists = calculateKilo(gameData.Lists, 0)
+gameData.Reviews = calculateKilo(gameData.Reviews, 0)
+
+train_test_split
