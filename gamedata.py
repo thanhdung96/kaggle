e240dc4ab2931667a783e7cdc8d1c03966f374b2
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-07-18 21:33:39 +0700

    Preprocessed release date feature.
    Modifications qui seront validées :
    	modifié :         backloggd_games.csv
    	nouveau fichier : backloggd_games_bak.csv
    	modifié :         gamedata.py

--- gamedata.py
+++ gamedata.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from ast import literal_eval
 from pandas.core.series import Series
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 
 def calculateKilo(col: Series, naReplacemane: int = 0) -> Series:
@@ -37,4 +39,27 @@ gameData.Developers = parseList(gameData.Developers, 'category')
 gameData.Platforms = parseList(gameData.Platforms, 'category')
 gameData.Genres = parseList(gameData.Genres, 'category')
 
-print(gameData.tail(20).Genres)
+# preprocess release date
+gameData.Release_Date = pd.to_datetime(gameData.Release_Date, format='%b %d, %Y')
+print(gameData.Release_Date)
+
+# encoding data
+labelEncoder = LabelEncoder()
+gameData.Title = labelEncoder.fit_transform(gameData.Title)
+
+# print(gameData.columns)
+# print('---------')
+# y = gameData.Rating
+#
+# train_X, test_X, train_y, test_y = train_test_split(
+#     gameData,
+#     y,
+#     train_size=0.9,
+#     test_size=0.1,
+#     random_state=0,
+#     shuffle=True
+# )
+#
+# featModel = RandomForestRegressor()
+# featModel.fit(train_X, train_y)
+# print(featModel.feature_importances_)
